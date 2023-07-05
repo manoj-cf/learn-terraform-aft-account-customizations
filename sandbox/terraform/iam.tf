bddee249bfa1b19e49290077f275216cff7ae126
Manoj Patil <manoj.patil@baptisthealth.net>
2023-07-05 12:00:17 +0530

    Added IAM code

--- sandbox/terraform/iam.tf
+++ sandbox/terraform/iam.tf
@@ -1,30 +1,9 @@
-#data "aws_caller_identity" "currentUser" {}
+################# Create Group #################
+# resource "aws_iam_group" "sys_analysts" {
+#   name = "System-Analysts"
+#   path = "/"
+# }
 
-resource "aws_iam_group_policy" "s3_policy" {
-  name  = "s3_policy"
-  group = aws_iam_group.sys_analysts.name
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "s3:*",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-}
+################# Create Permission Set #################
 
-resource "aws_iam_group" "sys_analysts" {
-  name = "System-Analysts"
-  path = "/"
-}
-
-resource "aws_iam_user_group_membership" "IT_Membership" {
- user = data.aws_caller_identity.current.user_id
- groups = [
-   aws_iam_group.sys_analysts.name
- ]
-}
+################# Attach Group and Permission Set to Account #################
