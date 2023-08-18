07904c35085dc38fb22ddce6b035cce57ef6857b
Manoj Patil <manoj.patil@baptisthealth.net>
2023-08-18 14:33:33 +0530

    Clean NW acc

--- network/terraform/s3.tf
+++
@@ -1,6 +0,0 @@
-data "aws_caller_identity" "current" {}
-
-resource "aws_s3_bucket" "network_bucket" {
-  bucket = "aft-network-${data.aws_caller_identity.current.account_id}"
-  acl    = "private"
-}
--- network/terraform/ssm.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_ssm_parameter" "ssmparameteraccid" {
-  name  = "/accID"
-  type  = "String"
-  value = "${data.aws_caller_identity.current.account_id}"
-}
