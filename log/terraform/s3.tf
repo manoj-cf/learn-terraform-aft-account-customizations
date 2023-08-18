b90548843334ddf0b392ced429950552f735c420
Manoj Patil <manoj.patil@baptisthealth.net>
2023-08-18 12:25:16 +0530

    Updated for Log acc

--- log/terraform/s3.tf
+++ log/terraform/s3.tf
@@ -1,5 +1,6 @@
 data "aws_caller_identity" "current" {}
 
+############ S3 bucket ###########
 resource "aws_s3_bucket" "log_bucket" {
   bucket = "aft-log-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
