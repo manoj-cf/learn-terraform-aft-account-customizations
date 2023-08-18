c256db45ae11c36a0f33c3730b1e9877990e5c1e
Manoj Patil <manoj.patil@baptisthealth.net>
2023-08-18 14:17:30 +0530

    Clean for Log acc

--- log/terraform/s3.tf
+++
@@ -1,7 +0,0 @@
-data "aws_caller_identity" "current" {}
-
-############ S3 bucket ###########
-resource "aws_s3_bucket" "log_bucket" {
-  bucket = "aft-log-${data.aws_caller_identity.current.account_id}"
-  acl    = "private"
-}
