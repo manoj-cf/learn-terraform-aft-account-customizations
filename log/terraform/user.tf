bf5514cb498eddd354ccf67663eac3549784f3c8
Manoj Patil <manoj.patil@baptisthealth.net>
2023-08-18 17:41:28 +0530

    Added code for policy arn

--- log/terraform/user.tf
+++ log/terraform/user.tf
@@ -1,7 +1,3 @@
-data "aws_iam_policy" "awsadministratoraccess_policy" {
-  arn = "arn:aws:iam::aws:policy/AWSAdministratorAccess"
-}
-
 # resource "aws_iam_policy_attachment" "attach-policy" {
 #   name       = "sagemaker-policy-attachment"
 #   users      = [aws_iam_user.user.name]
