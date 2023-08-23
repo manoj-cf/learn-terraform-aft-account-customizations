1f0bc656ceeb187020a80f9ee23e37e8ce6f39b2
Manoj Patil <manoj.patil@baptisthealth.net>
2023-08-23 14:06:03 +0530

    Added code for policy arn

--- log/terraform/data.tf
+++ log/terraform/data.tf
@@ -1,3 +1,4 @@
 data "aws_iam_policy" "awsadministratoraccess_policy" {
-  arn = "arn:aws:iam::aws:policy/AWSAdministratorAccess"
+  #arn = "arn:aws:iam::aws:policy/AWSAdministratorAccess"
+  arn = "arn:aws:sso:::permissionSet/ssoins-72237210185a1c22/ps-d8218cb6d461b51e"
 }
