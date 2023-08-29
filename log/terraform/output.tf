dcf257587df70e95cb61bf97f27bef70239b8e78
Manoj Patil <manoj.patil@baptisthealth.net>
2023-08-29 11:10:01 +0530

    Updated for IAM 1

--- log/terraform/output.tf
+++
@@ -1,7 +0,0 @@
-output "awsadministratoraccess_policy_arn" {
-  value = data.awsadministratoraccess_policy.arn
-}
-
-output "aws_caller_identity_current" {
-  value = data.aws_caller_identity.current
-}
--- log/terraform/user.tf
+++
@@ -1,5 +0,0 @@
-# resource "aws_iam_policy_attachment" "attach-policy" {
-#   name       = "sagemaker-policy-attachment"
-#   users      = [aws_iam_user.user.name]
-#   policy_arn = data.aws_iam_policy.sagemaker_policy.arn
-# }
